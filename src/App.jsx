3aa22258d6eff536bb0ac49e20d6ea951cf2fdcc
Tommy Burns <thomasburnsbotchwey@gmail.com>
2023-04-07 19:11:15 +0000

    components created and images added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import "./App.css";
+import {
+  About,
+  Hero,
+  Footer,
+  Navbar,
+  Specials,
+  Testimonials,
+} from "./components";
 
 function App() {
   return (
     <div className="App">
       <h2>Welcome To</h2>
       <h1>Little Lemon Restaurant</h1>
+      <Navbar />
+      <Hero />
+      <Specials />
+      <About />
+      <Testimonials />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import About from "./About";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Specials from "./Specials";
+import Testimonials from "./Testimonials";
+
+export { About, Footer, Hero, Navbar, Specials, Testimonials };
